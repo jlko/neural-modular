7fbe694e9686cf79e21b3883a74dff50fed775e0
Jannik Kossen <49904214+jlko@users.noreply.github.com>
2022-01-11 15:09:42 +0100

    update models

--- neural/models.py
+++ neural/models.py
@@ -32,65 +32,6 @@ class MLP(nn.Module):
         return self.block(x)
 
 
-# TODO ResNetMLP
-# class ResNetMLP(nn.Module):
-#     def __init__(self, cfg, data_in, data_out):
-#         super().__init__()
-
-#         p = cfg.dropout_p
-
-#         nonlin = nn.LeakyReLU
-#         bn = nn.BatchNorm1d
-
-#         self.block_1 = nn.Sequential(*[
-#             nn.Linear(data_in, 200),
-#             bn(200),
-#             nonlin(),
-#             nn.Linear(200, 200),
-#             bn(200),
-#             nonlin(),
-#             nn.Linear(200, 200),
-#             bn(200),
-#             ])
-
-#         self.block_2 = nn.Sequential(*[
-#             nn.Linear(200, 200),
-#             bn(200),
-#             nonlin(),
-#             nn.Linear(200, 50),
-#             bn(50),
-#             ])
-
-#         self.block_3 = nn.Sequential(*[
-#             nn.Linear(50, 50),
-#             bn(50),
-#             nonlin(),
-#             nn.Linear(50, 50),
-#             bn(50),
-#             nonlin(),
-#             nn.Dropout(p),
-#             nn.Linear(50, data_out),
-#             ])
-
-#         self.blocks = [self.block_1, self.block_2, self.block_3]
-
-#         self.linear_1 = nn.Linear(data_in, 200)
-#         self.linear_2 = nn.Linear(data_in, 50)
-#         self.linears = [
-#             self.linear_1, self.linear_2, None]
-
-#         # todo add bias
-#     def forward(self, x):
-#         out = x
-#         for block, linear in zip(self.blocks, self.linears):
-#             if linear is not None:
-#                 out = block(out) + linear(x)
-#             else:
-#                 out = block(out)
-#         return out
-
-# TODO LSTM
-
 class ResNetMLP(nn.Module):
     def __init__(self, cfg, data_in, data_out):
         super().__init__()
@@ -142,3 +83,28 @@ class ResNetMLP(nn.Module):
                 out = block(out)
         return out
 
+
+class LSTM(nn.Module):
+    def __init__(self, cfg, data_in, data_out):
+        super().__init__()
+
+        self.model = nn.LSTM(
+            input_size=data_in,
+            hidden_size=cfg.hidden_size,
+            num_layers=cfg.num_layers,
+            dropout=cfg.dropout_p,
+            batch_first=True)
+
+        self.linear = nn.Linear(cfg.num_layers * cfg.hidden_size, data_out)
+
+    def forward(self, x):
+        output, (h_n, c_n) = self.model(x)
+
+        # batch dim first
+        h_n = h_n.transpose(0, 1)
+        # flatten hidden states
+        h_n = h_n.reshape(x.shape[0], -1)
+        # project to out dim
+        out = self.linear(h_n)
+
+        return out
