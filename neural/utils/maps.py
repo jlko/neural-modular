d8474366d77556a69dff801fe0f769adda8f5285
Jannik Kossen <49904214+jlko@users.noreply.github.com>
2022-01-11 15:10:03 +0100

    Update maps.py

--- neural/utils/maps.py
+++ neural/utils/maps.py
@@ -1,6 +1,6 @@
 """Map strings to classes."""
 from neural.datasets import RandomLGSSM
-from neural.models import MLP, ResNetMLP
+from neural.models import MLP, ResNetMLP, LSTM
 
 dataset = dict(
     RandomLGSSM=RandomLGSSM,
@@ -9,4 +9,5 @@ dataset = dict(
 model = dict(
     MLP=MLP,
     ResNetMLP=ResNetMLP,
+    LSTM=LSTM,
 )
